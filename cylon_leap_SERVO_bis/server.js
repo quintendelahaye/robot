6a8f05a2bf2b8286f2a45950232bb13614db2f40
Your Name Herequintendelahaye <quinten_delahaye@hotmail.com>
2014-12-03 00:03:27 +0100

    double servo test

--- cylon_leap_SERVO_bis/server.js
+++ cylon_leap_SERVO_bis/server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 var Cylon = require('cylon');
 
-var handX = 0, angle = 0;
+var yaw = 0, pinch = 0, servo_angle = 0, servo2_angle = 0, frameCounter = 0;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,17 +18,36 @@ Cylon.robot({
 
   devices: {
     leapmotion: { driver: 'leapmotion', connection: 'leap' },
-    servo: { driver: 'servo', pin: 3, connection: 'arduino' }
+    servo: { driver: 'servo', pin: 3, connection: 'arduino' },
+    servo2: { driver: 'servo', pin: 9, connection: 'arduino' }
   },
 
   work: function(my) {
+
+
     my.leapmotion.on('frame', function(frame) {
-      if(frame.hands.length > 0) {
-        handX = frame.hands[0].palmPosition[0];
-        angle = (handX).fromScale(-255, 255).toScale(0, 180);
-        my.servo.angle(angle);
+
+        if(frame.hands.length > 0) {
+
+        // console.log('duim top positie -> ' + frame.fingers[0].tipPosition[0] + '\n' + 'index top positie ->' + frame.fingers[1].tipPosition[0]);
+        // console.log('afstand tussen duim en index -> ' + frame.fingers[0].tipPosition[0] + frame.fingers[1].tipPosition[0]);
+
+        console.log(frame.hands[0].pinchStrength);
+
+        // pinch = frame.hands[0].palmPosition[0];        
+
+        pinch = frame.hands[0].pinchStrength;
+        servo_angle = (pinch).fromScale(0, 1).toScale(0, 100);
+        my.servo.angle(servo_angle);     
+
+        yaw = frame.hands[0].yaw();
+        servo2_angle = (yaw).fromScale(-2,2).toScale(0, 180);
+        my.servo2.angle(servo2_angle);
       }
-    });
+
+      
+    
+  });
   }
 }).start();
 
